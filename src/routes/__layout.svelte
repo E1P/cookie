7ce492893b507556cb4b89ec8e30ccb30916e786
E1P <ewan.pearce@gmail.com>
2022-03-31 14:59:37 +0100

    Add top-level layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '$lib/css/common.css';
+</script>
+
+<div class="navbar" />
+<slot />
+
+<style>
+	.navbar {
+		margin: 5px 0;
+		padding: 0;
+		width: 100%;
+		height: 2vh;
+		background-color: darkgray;
+	}
+</style>
