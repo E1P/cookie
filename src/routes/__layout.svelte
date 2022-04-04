215925b9546e5a7cab3efad21908a989b88e7ffe
E1P <ewan.pearce@gmail.com>
2022-04-04 16:09:26 +0100

    Add normalize css reset file and import to route layout.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,9 @@
 <script context="module">
-	// import '$lib/css/common.css';
 	import '@fontsource/roboto-condensed';
+</script>
+
+<script>
+	import '$lib/css/normalize-220404.css';
 </script>
 
 <div class="navbar" />
@@ -9,17 +12,13 @@
 </main>
 
 <style>
-	/* @import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap'); */
-
 	main {
 		font-family: 'Roboto Condensed', Arial, Helvetica, sans-serif;
 		border: 2px solid darkorange;
 		padding: 10px;
-		margin: 0;
 	}
 
 	.navbar {
-		margin: 0;
 		padding: 0;
 		width: 100%;
 		height: 2vh;
